92411f70a38776b0fbb801b99f0bed5d97b5077f
Sebastian Kumór <thekumor@gmail.com>
2023-12-02 12:14:36 +0100

    Created a GLFW window

--- Engine/source/main.cpp
+++ Engine/source/main.cpp
@@ -1,7 +1,34 @@
 #include <GLFW/glfw3.h>
+#include <iostream>
 #include "engine.h"
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+#define WINDOW_TITLE "Test"
 
 int main()
 {
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Test", nullptr, nullptr);
+	if (!glfwInit())
+	{
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+	if (!window)
+	{
+		std::cerr << "Failed to create a GLFWwindow instance" << std::endl;
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
